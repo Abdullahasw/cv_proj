d68a851319c3f9f46b44f48f59846f4f1fb20e0b
abdullah <abdullahasmw@gmail.com>
2024-05-12 21:50:04 +0300

    first commit

--- apps/cvmodule/urls.py
+++ apps/cvmodule/urls.py
@@ -2,5 +2,7 @@ from django.urls import path,include
 from apps.cvmodule import views
 
 urlpatterns = [
-    path('',views.index,name='index')
-]
+    path('',views.index,name='index'),
+     path('imag',views.imag),
+     path('book/<int:bId>',views.book)
+     ]
--- apps/cvmodule/views.py
+++ apps/cvmodule/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from apps.cvmodule import views
 def index(request):
 # render the appropriate template for this request
+#just another comment
  return render(request, 'cvmodule/index.html')
+
+def imag(request):
+    #redirect('/')#go to the main one
+    return render(request, 'cvmodule/imag.html')
+
+def book(request,bId):
+    #return redirect("/")
+    book={'id':12,'title':'mynewbook','auther':'abdullah'}
+    book={'id':34,'title':'myoldbook','auther':'abdullah(old)'}
+    targetbook=None
+    if book['id']==bId:targetbook=book
+    if book['id']==bId:targetbook=book
+    
+    if targetbook==None: return redirect("/imag")
+    context={'book':targetbook}
+    return render(request,'cvmodule/book.html',context)
