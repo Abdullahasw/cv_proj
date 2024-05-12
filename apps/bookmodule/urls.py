322f1990ba67370db13f03cec0fce6b74935774b
abdullah alwehaibi <60135844+Abdullahasw@users.noreply.github.com>
2024-05-12 03:25:39 +0300

    Delete apps directory

--- apps/bookmodule/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path,include
-from apps.bookmodule import views
-urlpatterns = [
-    path('',views.index,name='index')
-]
--- apps/bookmodule/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-def index(request):
-    # render the appropriate template for this request
-    return render(request, 'bookmodule/index.html')
